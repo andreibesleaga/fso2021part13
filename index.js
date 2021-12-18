8816e6e473319fa26f39d9b3e69a09ce5a30f3a8
Andrei <33538982+andreibesleaga@users.noreply.github.com>
2021-12-18 12:53:22 +0200

    task 13.4 - web application

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
 
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+// connect to PostgreSQL
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
@@ -10,18 +15,30 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    for(const blog of blogs) {
-        console.log(blog.author + ': ' + blog.title + ', ' + blog.likes + ' likes')
-    }
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
+// list all blogs
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+  res.json(blogs)
+})
 
-main()
+// save new blog entry
+app.post('/api/blogs', async (req, res) => {
+  let author = req.body.author
+  let url = req.body.url
+  let title = req.body.title
+  let likes = req.body.likes
+  const blogs = await sequelize.query(`INSERT INTO blogs (author,url,title,likes) VALUES ('${author}','${url}','${title}','${likes}')`, { type: QueryTypes.INSERT })
+  res.json(blogs)
+})
+
+// delete a blog entry
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blogs = await sequelize.query("DELETE FROM blogs WHERE id=" + req.params.id, { type: QueryTypes.DELETE })
+  res.json(blogs)
+})
+
+// strt server
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
